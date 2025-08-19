749c911e396f33a2138107be6d8426f2d145259d
belleissns <belleis.sns@gmail.com>
2025-08-18 22:05:02 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,10 @@
-/* Lye, Dye & DIY - offline cache */
-const CACHE_VERSION = 'v1.0.0';const CACHE_NAME = "lye-dye-diy-v3"; // bump this
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open('bel-lei-v1').then(c=>c.addAll([
+    './','./index.html','./manifest.webmanifest'
+  ])));
 });
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).then(resp => {
-      const copy = resp.clone();
-      caches.open(CACHE_NAME).then(c => c.put(event.request, copy));
-      return resp;
-    }).catch(() => caches.match(event.request).then(r => {
-      if (event.request.mode === 'navigate') return caches.match('./index.html');
-      return r;
-    }))
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request))
   );
 });
